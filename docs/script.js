0a035978db06d025ad0c81b4787a2db73f7cdf98
Carlos Romero <erenestor@hotmail.com>
2024-11-05 13:27:45 -0600

    erenestor@hotmail.com

---
+++ docs/script.js
@@ -0,0 +1,1 @@
+document.getElementsByTagName('h1')[0].onmouseout=function(){this.innerText='Active Heading'; this.style.color='Black'}
---
+++ docs/try-test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+ Te piso
+<?php
+echo phpversion();
+echo "La godina es homosexual" ;
+
+?> 
+
+</body>
+</html>
